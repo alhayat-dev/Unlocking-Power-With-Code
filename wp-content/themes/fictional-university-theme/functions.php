84fba8a580f45d89d7260198a3e681e44c4fbb41
Mudasser Hayat <mudasser@royalbathrooms.co.uk>
2021-06-11 16:41:34 +0500

    made search button function on click and keyup event listener.

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -37,9 +37,9 @@ function university_files()
     if (strstr($_SERVER['SERVER_NAME'], 'local.fictional.university.com')){
         wp_enqueue_script('main-unversity-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
     }else{
-        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
-        wp_enqueue_script('main-unversity-js', get_theme_file_uri('/bundled-assets/scripts.48e59d7e7e54a2111a4d.js'), NULL, '1.0', true);
-        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.48e59d7e7e54a2111a4d.css'));
+        wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.1fa169383e64a33bfd0c.js'), NULL, '1.0', true);
+        wp_enqueue_script('main-unversity-js', get_theme_file_uri('/bundled-assets/scripts.521bb7ac4127d4bf2879.js'), NULL, '1.0', true);
+        wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.521bb7ac4127d4bf2879.css'));
     }
 }
 
